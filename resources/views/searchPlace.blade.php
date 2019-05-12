fc48257b29b8ccb4426b1c87320343a56b9aec3a
bkasteller <bryan.kasteller@gmail.com>
2019-05-12 13:00:00 +0200

    petite modif

--- resources/views/searchPlace.blade.php
+++ resources/views/searchPlace.blade.php
@@ -5,42 +5,46 @@
     <div class="row justify-content-center" style="margin-top: 30px;">
         <div class="col-md-8">
             <div class="card">
-                @foreach ($places as $place)
-                    <p>
-                        <B>Place N°{{ $place->id }}</B>
-                        <br>
-                        @if ( $place->occupied() )
-                            Assigned to : {{ $place->user()->name() }}
-                        @else
-                            No user is assigned to this place.
-                        @endif
-                        <br>
-                        Place status :
-                        @if ( $place->available )
-                            <font color="green">Open</font>
-                        @else
-                            <font color="red">Close</font>
-                        @endif
-                        <br>
+                <div class="card-header">Parking Places</div>
 
-                        <a href="{{ route('place.edit', $place) }}">
-                            <button type="button" class="btn btn-outline-success">
-                                Informations
-                            </button>
-                        </a>
+                <div class="card-body">
+                    @foreach ($places as $place)
+                        <p>
+                            <B>Place N°{{ $place->id }}</B>
+                            <br>
+                            @if ( $place->occupied() )
+                                Assigned to : {{ $place->user()->name() }}
+                            @else
+                                No user is assigned to this place.
+                            @endif
+                            <br>
+                            Place status :
+                            @if ( $place->available )
+                                <font color="green">Open</font>
+                            @else
+                                <font color="red">Close</font>
+                            @endif
+                            <br>
 
-                        <a href="{{ route('place.update', $place) }}">
-                            <button type="button" class="btn">
-                                {{ $place->available ? 'Close' : 'Open' }}
-                            </button>
-                        </a>
-                    </p>
-                @endforeach
-                <a href="{{ route('place.create') }}" style="width:0">
-                    <button type="button" class="btn">
-                        + Add one place
-                    </button>
-                </a>
+                            <a href="{{ route('place.edit', $place) }}">
+                                <button type="button" class="btn btn-outline-success">
+                                    Informations
+                                </button>
+                            </a>
+
+                            <a href="{{ route('place.update', $place) }}">
+                                <button type="button" class="btn">
+                                    {{ $place->available ? 'Close' : 'Open' }}
+                                </button>
+                            </a>
+                        </p>
+                    @endforeach
+                    <a href="{{ route('place.create') }}" style="width:0">
+                        <button type="button" class="btn">
+                            + Add one place
+                        </button>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
